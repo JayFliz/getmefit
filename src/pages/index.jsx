1763a958eb419c8d6cf77a1e1092d5ef18bd2226
Siebe Van Dijck <siebe@thisisundefined.com>
2020-03-20 08:10:48 +0000

    updated copy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,25 +9,13 @@ const Index = () => (
     <Status />
     <View title="Firebase Authentication Starter">
       <p>
-      👋 This is a simple demo of creating dynamic pages with Gatsby that require <b>user authentication</b> and using <b>Firebase</b> as authentication provider. 
-      </p>  
-      <br/>
-      <p>
-       📚 It uses concepts from the
-        {` `}
-        <a className="underline text-blue-500" href="https://www.gatsbyjs.org/docs/client-only-routes-and-user-authentication/">
-          client-only routes section
-        </a>
-        {` `} and {` `}
-        <a className="underline text-blue-500" href="https://www.gatsbyjs.org/tutorial/authentication-tutorial">
-          making a site with user authentication
-        </a>
-        {` `}
-        tutorial in the official gatsby doc.
+      👋 Hello There<br/>
+      Group Fitness Instructor & Personal Trainer Profiles
       </p>
       <br/>
+      <br/>
       <p>
-        This is the public home, and here it is a private route to start:  
+        This is the public home, and here it is a private route to start:
         <br/>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline float-right" type="button">
           <Link to="/app/profile">Go to your profile</Link>
